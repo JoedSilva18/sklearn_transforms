7675524a279696306a872479988e22550b8aff24
Joed Vicente Da Silva <joed@mcity15.aquarius.cpqd.com.br>
2020-08-26 17:25:43 -0300

    Adicionando funcoes

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from imblearn.over_sampling import SMOTE
 from sklearn.base import BaseEstimator, TransformerMixin
-
+from sklearn import preprocessing
+from sklearn.preprocessing import StandardScaler
 
 class DropColumns(BaseEstimator, TransformerMixin):
     def __init__(self, columns):
@@ -39,3 +40,47 @@ class SmoteResample(object):
         X_resampled, y_resampled = SMOTE(random_state=42).fit_resample(X, y)
         X_resampled = pd.DataFrame(X_resampled, columns=X.columns)
         return X_resampled, y_resampled
+
+class MultiColumnLabelEncoder:
+
+    def __init__(self, columns=None):
+        self.columns = columns # array of column names to encode
+
+
+    def fit(self, X, y=None):
+        self.encoders = {}
+        columns = X.columns if self.columns is None else self.columns
+        for col in columns:
+            self.encoders[col] = preprocessing.LabelEncoder().fit(X[col])
+        return self
+
+
+    def transform(self, X):
+        output = X.copy()
+        columns = X.columns if self.columns is None else self.columns
+        for col in columns:
+            output[col] = self.encoders[col].transform(X[col])
+        return output
+
+
+    def fit_transform(self, X, y=None):
+        return self.fit(X,y).transform(X)
+
+
+    def inverse_transform(self, X):
+        output = X.copy()
+        columns = X.columns if self.columns is None else self.columns
+        for col in columns:
+            output[col] = self.encoders[col].inverse_transform(X[col])
+        return output
+
+class ScalerData():
+    def __init__(self):
+        pass
+
+    def fit(self, X):
+        scaler = StandardScaler()
+        X_resampled = scaler.fit_transform(X)
+        X_resampled = pd.DataFrame(X)
+
+        return X_resampled
